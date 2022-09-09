83cd3b21fbed6154e77bafad1224fd88575f98ea
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-09 10:17:41 +0300

    Add test for fizzbuzz

--- spec/solever_spec.rb
+++ spec/solever_spec.rb
@@ -3,10 +3,6 @@ require './solver'
 describe Solver do
   describe '#factorial' do
 
-    # before(:each) do
-    #   @solver = Solver.new
-    # end
-
     # Test 1 => passing a negative integer
     it 'raises an exception when given a negative number' do
         solver = Solver.new
@@ -33,4 +29,26 @@ describe Solver do
       expect(new_str.reverse('hello')).to eq 'olleh'
     end
   end
+
+
+  describe '#fizzbuzz' do
+    #test 1 => when given a number that is divisible by 3
+    it 'returns fizz when given number is divisible by 3' do
+      test1 = Solver.new
+      expect(test1.fizzbuzz(3)).to eq('fizz')
+    end
+   
+    #test 2 => when given a number that is divisible by 5
+    it 'returns buzz when given number is divisible by 5' do
+      test2 = Solver.new
+      expect(test2.fizzbuzz(5)).to eq('buzz')
+    end
+
+    #test 3 => when given a number that is divisible by 3 and 5
+    it 'returns fizzbuzz when given number is divisible by 3 and 5' do
+      test3 = Solver.new
+      expect(test3.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    end
+
 end
